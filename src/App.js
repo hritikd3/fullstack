5df3706c201ceda12f5dca4ad7b7d7d35d7fa698
hritikd3 <hritik@intelliapt.com>
2022-05-02 00:11:47 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
 function App() {
-  return <h4>Mr.furniturewala</h4>
+  return <h4>Mr..furniturewala</h4>
 }
 
 export default App
