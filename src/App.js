6a4a63c4ef440de79ed6fb219583ffafe5476385
hritikd3 <hritik@intelliapt.com>
2022-05-11 12:18:08 +0530

    Routers defined

--- src/App.js
+++ src/App.js
@@ -18,28 +18,8 @@ function App() {
     <Router>
       <Navbar />
       <Sidebar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/cart">
-          <Cart />
-        </Route>
-        <Route exact path="/products">
-          <Products />
-        </Route>
-        <Route exact path="/products/:id" children={<SingleProduct />} />
-        <Route exact path="/checkout">
-          <Checkout />
-        </Route>
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
-        <Footer />
+     
+        <Footer /> 
     </Router>
   );
 }
