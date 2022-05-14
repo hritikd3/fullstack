f2e08d19937f42870a3fe0716d91f84c95702b52
1somya <1somya.agr@gmail.com>
2022-05-15 01:42:53 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+import React from 'react';  
+import ReactDOM from 'react-dom'; 
+import { BrowserRouter as Router,Switch, Routes, Route } from "react-router-dom";
 import Home from './HomePage'
 import Products from './ProductsPage'
 import SingleProduct from './SingleProductPage'
@@ -7,4 +10,18 @@ import Error from './ErrorPage'
 import Checkout from './CheckoutPage'
 import PrivateRoute from './PrivateRoute'
 
-export { Home, Products, SingleProduct, About, Cart, Error,Checkout,PrivateRoute };
+//export { Home, Products, SingleProduct, About, Cart, Error,Checkout,PrivateRoute };
+const routing = (  
+  <Router>  
+      <Switch>  
+         <Route path="/" component={Home} />  
+         <Route path="/about" component={About} />  
+         <Route path="/products" component={Products} />   
+         <Route path="/cart" component={Cart} /> 
+         <Route path="/checkout" component={Checkout} /> 
+         <Route path="/error" component={Error} />   
+      </Switch>  
+  </Router>  
+)  
+  
+ReactDOM.render(routing, document.getElementById('root'));  
