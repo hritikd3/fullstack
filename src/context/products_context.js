5d57c4b761e13a78aa8b12807224e54987d75c35
hritikd3 <hritik@intelliapt.com>
2022-05-23 11:37:57 +0530

    productApi completed

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,8 +15,15 @@ import {
 
 const initialState = {
   //checking that our sidebar is open or not initial value is false
-  isSidebarOpen: true,
-}
+  isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
+};
 
 const ProductsContext = React.createContext()
 //redux is playing role here 
@@ -30,6 +37,13 @@ const closeSidebar = () => {
   dispatch({ type: SIDEBAR_CLOSE });
 };
 
+const fetchProducts= async(url)=>{
+  const response=await axios.get(url)
+  console.log(response)
+}
+useEffect(()=>{
+fetchProducts(url);
+},[])
 //useEfffects hooks has been defined
 // useEffect(()=>{
 //   openSidebar()
