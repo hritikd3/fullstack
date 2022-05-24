46f4279efcb234e2c0107dee584cb5cf0515220f
hritikd3 <hritik@intelliapt.com>
2022-05-24 15:30:56 +0530

    Home page spaces fix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { FeaturedProducts, Hero, Services, Contact } from '../components'
 const HomePage = () => {
-  return <main>
-    <Hero />
-    <FeaturedProducts />
-    <Services />
-    <Contact />
-  </main>
+  return (
+    <main>
+      <br /> <br />
+      <Hero />
+      <br />
+      <FeaturedProducts />
+      <Services />
+      <Contact />
+    </main>
+  );
 }
 
 export default HomePage
