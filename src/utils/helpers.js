d90b6a74e391718578b47d5c8a0da5239765813d
hritikd3 <hritik@intelliapt.com>
2022-05-24 15:23:05 +0530

    format of price for indian currency

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  return new Intl.NumberFormat("en-IN", {
+    maximumFractionDigits: 2,
+    style: "currency",
+    currency: "INR",
+  }).format(number / 100);
+};
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = () => {};
