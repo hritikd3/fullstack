ab56d140d32d78e9290a5296879add28f39bf1d2
Hritik d3 <hritikdangi3@gmailcom>
2022-05-25 10:23:20 +0530

    refactored

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
-import { useParams, useHistory } from 'react-router-dom'
-import { useProductsContext } from '../context/products_context'
-import { single_product_url as url } from '../utils/constants'
-import { formatPrice } from '../utils/helpers'
+import React, { useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { useProductsContext } from "../context/products_context";
+import { single_product_url as url } from "../utils/constants";
+import { formatPrice } from "../utils/helpers";
 import {
   Loading,
   Error,
@@ -10,13 +10,13 @@ import {
   AddToCart,
   Stars,
   PageHero,
-} from '../components'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+} from "../components";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const SingleProductPage = () => {
   const { id } = useParams();
-  const history= useHistory();
+  const history = useHistory();
   const {
     single_product_loading: loading,
     single_product_error: error,
@@ -31,8 +31,9 @@ const SingleProductPage = () => {
   useEffect(() => {
     if (error) {
       setTimeout(() => {
-        console.log(error)
-        history.push('/');
+        console.log(error);
+
+        history.push("/");
       }, 2500);
     }
     // eslint-disable-next-line
@@ -58,37 +59,37 @@ const SingleProductPage = () => {
   return (
     <Wrapper>
       <PageHero title={name} product />
-   <div className='section section-center page'>
-        <Link to='/products' className='btn'>
+      <div className="section section-center page">
+        <Link to="/products" className="btn">
           back to products
         </Link>
-        <div className='product-center'>
+        <div className="product-center">
           <ProductImages images={images} />
-          <section className='content'>
+          <section className="content">
             <h2>{name}</h2>
             <Stars stars={stars} reviews={reviews} />
-            <h5 className='price'>{formatPrice(price)}</h5>
-            <p className='desc'>{description}</p>
-            <p className='info'>
+            <h5 className="price">{formatPrice(price)}</h5>
+            <p className="desc">{description}</p>
+            <p className="info">
               <span>Available : </span>
-              {stock > 0 ? 'In stock' : 'out of stock'}
+              {stock > 0 ? "In stock" : "out of stock"}
             </p>
-            <p className='info'>
+            <p className="info">
               <span>SKU :</span>
               {sku}
             </p>
-            <p className='info'>
+            <p className="info">
               <span>Brand :</span>
               {company}
             </p>
             <hr />
-            {stock >0 && <AddToCart />}
-        </section>
+            {stock > 0 && <AddToCart />}
+          </section>
         </div>
-        </div>
-        
-  </Wrapper>
-  )}
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.main`
   .product-center {
@@ -122,6 +123,6 @@ const Wrapper = styled.main`
       font-size: 1.25rem;
     }
   }
-`
+`;
 
-export default SingleProductPage
+export default SingleProductPage;
