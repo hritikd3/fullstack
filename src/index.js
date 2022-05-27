85d152ac5d597753c50984b0a471ff722cfbea49
hritikd3 <hritikdangi3@gmail.com>
2022-05-28 01:09:16 +0530

    updated with the provider

--- src/index.js
+++ src/index.js
@@ -7,16 +7,26 @@ import { FilterProvider } from './context/filter_context'
 import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
-
+// dev-6htm-9pd.us.auth0.com
+// Vk2hhNhKHfuGHlWciIeC1RAUJgObTmnO;
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider >
-<CartProvider >
+  <UserProvider>
 
-    <App />
-</CartProvider>
+  <Auth0Provider
+    domain="dev-6htm-9pd.us.auth0.com"
+    clientId="Vk2hhNhKHfuGHlWciIeC1RAUJgObTmnO"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>
 
-    </FilterProvider>
-  </ProductsProvider>,
+  </UserProvider>,
   document.getElementById("root")
 );
