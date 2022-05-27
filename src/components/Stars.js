d6e482bdcd80a424441d20e12fff23fa3067eff4
Hritik d3 <hritikdangi3@gmailcom>
2022-05-27 09:46:09 +0530

    refactored: stars.js

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,14 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 const Stars = ({ stars, reviews }) => {
-  
   const tempStars = Array.from({ length: 5 }, (_, index) => {
-    const number = index + 0.5
-  // console.log(stars, reviews);
+    const number = index + 0.5;
+    return (
+      <span key={index}>
+        {stars > number ? (
+          <BsStarFill />
+        ) : stars > index ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    );
+  });
   return (
     <Wrapper>
-      <div className="stars"></div>
-      <p className="reviews">({reviews} customer reviews)</p>{" "}
+      <div className="stars">{tempStars}</div>
+      <p className="reviews">({reviews} customer reviews)</p>
     </Wrapper>
   );
 };
