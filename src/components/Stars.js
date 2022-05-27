5c46640b1b17fbfe3c2c788ca95ed53b66baa85a
Hritik d3 <hritikdangi3@gmailcom>
2022-05-27 09:38:44 +0530

    changes

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,6 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 const Stars = ({ stars, reviews }) => {
+  
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5
   // console.log(stars, reviews);
   return (
     <Wrapper>
