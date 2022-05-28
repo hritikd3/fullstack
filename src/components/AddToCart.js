e5e3e3522d397d21775f2fb6b00574b7710eacba
hritikd3 <hritikdangi3@gmail.com>
2022-05-28 10:03:28 +0530

    counter problem fixed

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -5,11 +5,30 @@ import { FaCheck } from "react-icons/fa";
 import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 const AddToCart = ({ product }) => {
-  const { addToCart } = useCartContext();
+  const {addToCart}= useCartContext();
   // add to cart
   const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
 
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    });
+  };
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    });
+  };
   return (
     <Wrapper>
       <div className="colors">
@@ -32,11 +51,12 @@ const AddToCart = ({ product }) => {
         </div>
       </div>
       <div className="btn-container">
-        <Link
-          to="/cart"
-          className="btn"
-          onClick={() => addToCart(id, mainColor, product)}
-        >
+        <AmountButtons
+          increase={increase}
+          decrease={decrease}
+          amount={amount}
+        />
+        <Link to="/cart" className="btn" onClick={()=>addToCart(id,mainColor,amount,product)}>
           add to cart
         </Link>
       </div>
