54635b9814c36732941c2a1c36890ad09a2c6a39
hritikd3 <hritikdangi3@gmail.com>
2022-05-28 10:02:44 +0530

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,15 +7,16 @@ import { links } from '../utils/constants'
 import CartButtons from './CartButtons'
 import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
-const Nav = () => { 
+const Nav = () => {
   const { openSidebar } = useProductsContext();
+  const { myUser } = useUserContext();
   return (
     <NavContainer>
       <div className="nav-center">
         <div className="nav-header">
           <Link to="/">
-            <img src={logoo} alt="logo" />
-          </Link>  
+            <img src={logoo} alt="navbar logo" />
+          </Link>
           <button type="button" className="nav-toggle" onClick={openSidebar}>
             <FaBars />
           </button>
@@ -29,11 +30,11 @@ const Nav = () => {
               </li>
             );
           })}
-          
+          {myUser && (
             <li>
               <Link to="/checkout">checkout</Link>
             </li>
-          
+          )}
         </ul>
         <CartButtons />
       </div>
