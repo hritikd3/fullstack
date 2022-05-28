f268f7c5ad0d441a44beab7370ee510cd59be5b7
hritikd3 <hritikdangi3@gmail.com>
2022-05-28 10:03:15 +0530

    ternary condition stated

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -6,6 +6,7 @@ import { formatPrice } from '../utils/helpers'
 import { Link } from 'react-router-dom'
 const CartTotals = () => {
   const { total_amount, shipping_fee } = useCartContext();
+  const { myUser, loginWithRedirect } = useUserContext();
 
   return (
     <Wrapper>
@@ -22,12 +23,15 @@ const CartTotals = () => {
             order total :<span>{formatPrice(total_amount + shipping_fee)}</span>
           </h4>
         </article>
-        
+        {myUser ? (
           <Link to="/checkout" className="btn">
             proceed to checkout
           </Link>
-        
-   
+        ) : (
+          <button onClick={loginWithRedirect} className="btn">
+            login
+          </button>
+        )}
       </div>
     </Wrapper>
   );
